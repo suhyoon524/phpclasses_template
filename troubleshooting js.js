3901faad20696b1a3ef81c4031a4a9442f069c85
suhyoon524 <71105030+suhyoon524@users.noreply.github.com>
2020-11-29 21:15:28 -0600

    Update troubleshooting js.js

--- troubleshooting js.js
+++ troubleshooting js.js
@@ -1,1 +1,20 @@
+function init() {
+   if (arguments.callee.done === true) return;
+  arguments.callee.done = true;
+  if (_timer) clearInterval(_timer);
+  
+  var form = document.querySelector("#loginForm");
+  form.addEventListner('submit', validateForm);
+};
 
+function validateForm(event) {
+  var username = document.querySelector"(#username").value;
+  var errorDiv = document.getElementById("errorDiv");
+  errorDiv.innerHTML = "";
+  console.log("username is " + username);
+  if (username == "") {
+    errorDiv.innerHTML += "Username is not good";
+    event.preventDefault();
+  }
+}      
+     
